197977849116cab44d2e621288dc19a842ba0790
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-04-18 12:16:07 +0530

    Add files via upload

---
+++ static variable, method,block.c
@@ -0,0 +1,20 @@
+package room1;
+
+public class room1 {
+	
+		   static int x = 10;
+		   static int y;
+		   static void func(int z) {
+		      System.out.println("x = " + x);
+		      System.out.println("y = " + y);
+		      System.out.println("z = " + z);
+		   }
+		   static {
+		      System.out.println("Running static initialization block.");
+		      y = x + 5;
+		   }
+		   public static void main(String args[]) {
+		      func(8);
+		   }
+		}
+
