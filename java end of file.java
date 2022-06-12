275ac9c8ee389ad2a24bbb01ec37b4f0052cb137
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-06-12 23:22:49 +0530

    Update java end of file.java

--- java end of file.java
+++ java end of file.java
@@ -7,7 +7,6 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scan = new Scanner(System.in);
    int i = 0;
         while(scan.hasNext()){
