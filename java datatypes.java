d92e1e13ff8e209f02aca6956f8580cec9ce6c5b
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-07-24 16:19:53 +0530

    Update java datatypes.java

--- java datatypes.java
+++ java datatypes.java
@@ -16,7 +16,6 @@ class Solution{
             long x=sc.nextLong();
             System.out.println(x+" can be fitted in:");
             if(x>=-128 && x<=127)System.out.println("* byte");
-            //Complete the code
             if(x >= -Math.pow(2, 15) && x <= Math.pow(2, 15) - 1)
                 System.out.println("* short");
             if(x >= -Math.pow(2, 31) && x <= Math.pow(2, 31) - 1)
