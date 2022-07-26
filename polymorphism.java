71b900bc83be3fec56695655124ecd83c7857337
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-07-26 22:30:04 +0530

    Update polymorphism.java

--- polymorphism.java
+++ polymorphism.java
@@ -15,7 +15,6 @@ class Square extends Polygon {
 
 class Circle extends Polygon {
 
-  // renders circle
   public void render() {
     System.out.println("Rendering Circle...");
   }
@@ -24,11 +23,9 @@ class Circle extends Polygon {
 class Main {
   public static void main(String[] args) {
     
-    // create an object of Square
     Square s1 = new Square();
     s1.render();
 
-    // create an object of Circle
     Circle c1 = new Circle();
     c1.render();
   }
