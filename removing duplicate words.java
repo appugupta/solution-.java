6c28ee1fc95fd4407b6c006729a02e021a3450d4
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-07-29 22:48:28 +0530

    Update removing duplicate words.java

--- removing duplicate words.java
+++ removing duplicate words.java
@@ -13,12 +13,10 @@ public class DuplicateWord {
             for(int j = i+1; j < words.length; j++) {    
                 if(words[i].equals(words[j])) {    
                     count++;    
-                    //Set words[j] to 0 to avoid printing visited word    
                     words[j] = "0";    
                 }    
             }    
                 
-            //Displays the duplicate word if count is greater than 1    
             if(count > 1 && words[i] != "0")    
                 System.out.println(words[i]);    
         }    
